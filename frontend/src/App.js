525e93f83026d4f92c76de22fced944964f12837
T-WHB <brandon.twh@gmail.com>
2025-01-11 12:40:38 +0800

    Navbar routing working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,38 @@
 // src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
 import Login from './Login';
 import AppNavbar from './components/Navbar';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 
 function App() {
   return (
-    <div>
-      {/* <AppNavbar /> */}
-      <Login />
-    </div>
+    <Router>
+      <div>
+        <AppNavbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+        
+      </div>
+    </Router>
   );
+
+  // return (
+  //   <div>
+  //     <AppNavbar />
+  //     {/* <Login /> */}
+  //   </div>
+  // );
 }
+
 
 export default App;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,43 +1,35 @@
-// src/components/Navbar.js
 import React from 'react';
-import {
-  MDBNavbar, MDBContainer, MDBNavbarBrand, MDBNavbarToggler, MDBIcon,
-  MDBNavbarNav, MDBNavbarItem, MDBNavbarLink, MDBCollapse
-} from 'mdb-react-ui-kit';
+import { MDBNavbar, MDBContainer, MDBNavbarBrand, MDBNavbarNav, MDBNavbarItem, MDBNavbarLink } from 'mdb-react-ui-kit';
+import { Link } from 'react-router-dom';
 
-export default function AppNavbar() {
-  const [showNavRight, setShowNavRight] = React.useState(false);
-
+export default function Navbar() {
   return (
-    <MDBNavbar expand='lg' light bgColor='white'>
+    <MDBNavbar expand='lg' light bgColor='light'>
       <MDBContainer fluid>
-        <MDBNavbarBrand href='#'>Brand</MDBNavbarBrand>
-        <MDBNavbarToggler
-          type='button'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-          onClick={() => setShowNavRight(!showNavRight)}
-        >
-          <MDBIcon icon='bars' fas />
-        </MDBNavbarToggler>
-        <MDBCollapse navbar show={showNavRight}>
-          <MDBNavbarNav right fullWidth={false} className='mb-2 mb-lg-0'>
-            <MDBNavbarItem>
-              <MDBNavbarLink active aria-current='page' href='#'>
-                Home
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href='#'>Features</MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href='#'>Pricing</MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href='#'>About</MDBNavbarLink>
-            </MDBNavbarItem>
-          </MDBNavbarNav>
-        </MDBCollapse>
+        <MDBNavbarBrand>
+          <Link to="/" className="navbar-brand">BrandName</Link>
+        </MDBNavbarBrand>
+        <MDBNavbarNav right fullWidth={false} className='mb-2 mb-lg-0'>
+          <MDBNavbarItem>
+            {/* <MDBNavbarLink as={Link} to="/">Home</MDBNavbarLink> */}
+            <Link to="/" className="navbar-brand">Home</Link>
+          </MDBNavbarItem>
+          <MDBNavbarItem>
+            {/* <MDBNavbarLink as={Link} to="/about">About</MDBNavbarLink> */}
+            <Link to="/about" className="navbar-brand">About</Link>
+
+          </MDBNavbarItem>
+          <MDBNavbarItem>
+            {/* <MDBNavbarLink as={Link} to="/services">Services</MDBNavbarLink> */}
+            <Link to="/services" className="navbar-brand">Services</Link>
+
+          </MDBNavbarItem>
+          <MDBNavbarItem>
+            {/* <MDBNavbarLink as={Link} to="/contact">Contact</MDBNavbarLink> */}
+            <Link to="/contact" className="navbar-brand">Contact</Link>
+
+          </MDBNavbarItem>
+        </MDBNavbarNav>
       </MDBContainer>
     </MDBNavbar>
   );
