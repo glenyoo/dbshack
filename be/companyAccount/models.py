1ce0fe871588e536cd36fea75a3c5f6d23d26fe0
Glen Low <glen.low.2021@scis.smu.edu.sg>
2025-01-11 12:00:45 +0800

    basic view and model

--- be/companyAccount/models.py
+++ be/companyAccount/models.py
@@ -12,35 +12,3 @@ class CompanyAccount(models.Model):
 
     def __str__(self):
         return self.company_name
-
-# OutstandingRequest model
-class OutstandingRequest(models.Model):
-    REQUEST_TYPES = [
-        ('BUY', 'Buy'),
-        ('SELL', 'Sell'),
-    ]
-    requestor = models.ForeignKey(CompanyAccount, on_delete=models.CASCADE, related_name='requests_made')
-    target_company = models.ForeignKey(CompanyAccount, on_delete=models.CASCADE, related_name='requests_received')
-    carbon_unit_price = models.FloatField(default=0)
-    carbon_quantity = models.FloatField(default=0)
-    request_reason = models.TextField()
-    request_status = models.CharField(max_length=50, default='Pending Approval')
-    request_type = models.CharField(max_length=4, choices=REQUEST_TYPES)
-    created_datetime = models.DateTimeField(default=now)
-    updated_datetime = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"{self.requestor} -> {self.target_company} ({self.request_type})"
-    
-# RequestReceived model
-class RequestReceived(models.Model):
-    request = models.ForeignKey(OutstandingRequest, on_delete=models.CASCADE, related_name='alerts')
-    alert_datetime = models.DateTimeField()
-    alert_text = models.TextField()
-    alert_status = models.CharField(max_length=50)
-    alert_view_date = models.DateTimeField(null=True, blank=True)
-    created_datetime = models.DateTimeField(default=now)
-    updated_datetime = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"Alert for Request ID {self.request.id}"
--- be/outstandingRequest/views.py
+++ be/outstandingRequest/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import CompanyAccount, OutstandingRequest, RequestReceived
+from .serializers import OutstandingRequestSerializers
 
-# Create your views here.
+class ListOutstandingRequest(APIView):
+
+    def get(self, request, format=None):
+        accounts = OutstandingRequest.objects.all()
+        return Response(accounts)
